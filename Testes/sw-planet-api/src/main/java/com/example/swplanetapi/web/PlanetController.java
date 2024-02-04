f8b4c2bfe48c7ffcf8ac9a28a5fab60014201ec7
Matheus dos Santos <matheus.dso1996@gmail.com>
2024-02-03 21:54:09 -0300

    Fixed class "PlanetController.java" from "@RequestMapping("/planet") ~ @RequestMapping("/planets").

--- Testes/sw-planet-api/src/main/java/com/example/swplanetapi/web/PlanetController.java
+++ Testes/sw-planet-api/src/main/java/com/example/swplanetapi/web/PlanetController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
-@RequestMapping("/planet")
+@RequestMapping("/planets")
 public class PlanetController {
     @Autowired
     private PlanetService planetService;
