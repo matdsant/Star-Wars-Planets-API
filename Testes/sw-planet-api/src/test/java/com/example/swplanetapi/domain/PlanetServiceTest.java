b47bcd45e35b2c46c24fdb553d532be3a20fdc3b
Matheus dos Santos <matheus.dso1996@gmail.com>
2024-02-08 20:42:11 -0300

    Implement "SpringBootTest" for testing purposes (learning exercise)

--- Testes/sw-planet-api/src/test/java/com/example/swplanetapi/domain/PlanetServiceTest.java
+++ Testes/sw-planet-api/src/test/java/com/example/swplanetapi/domain/PlanetServiceTest.java
@@ -1,20 +1,29 @@
 package com.example.swplanetapi.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 import static com.example.swplanetapi.common.PlanetConstants.PLANET;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest(classes = PlanetService.class)
 public class PlanetServiceTest {
     @Autowired
     private PlanetService planetService;
 
+    @MockBean 
+    private PlanetRepository planetRepository;
+
     @Test
     public void createPlanet_WithValidData_ReturnsPlanet() {
-        // System Under Test
+        // AAA
+        // Arrange - Arruma os dados e prepara o Stub
+        when(planetRepository.save(PLANET)).thenReturn(PLANET);
+        // Act - Agir na operação e executa
         Planet sut = planetService.create(PLANET);
+        // Assert - Aferir se o sistema é o que se esperava
         assertThat(sut).isEqualTo(PLANET);
     }
 }
