db0e9c872182880285a4022f6b08d1c1ddd77050
Matheus dos Santos <matheus.dso1996@gmail.com>
2024-02-08 20:48:06 -0300

    Implement unit tests using Mockito (learning exercise, no longer using SpringBootTest)

--- Testes/sw-planet-api/src/test/java/com/example/swplanetapi/domain/PlanetServiceTest.java
+++ Testes/sw-planet-api/src/test/java/com/example/swplanetapi/domain/PlanetServiceTest.java
@@ -4,16 +4,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 import static com.example.swplanetapi.common.PlanetConstants.PLANET;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-@SpringBootTest(classes = PlanetService.class)
+@ExtendWith(MockitoExtension.class)
+//@SpringBootTest(classes = PlanetService.class)
 public class PlanetServiceTest {
-    @Autowired
+    //@Autowired
+    @InjectMocks
     private PlanetService planetService;
 
-    @MockBean 
+    //@MockBean
+    @Mock
     private PlanetRepository planetRepository;
 
     @Test
