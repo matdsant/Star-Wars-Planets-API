a4183aa01bd468bbe2d66557bb0708ef2a54a35f
Matheus dos Santos <matheus.dso1996@gmail.com>
2024-02-13 16:26:19 -0300

    Remove unused import (Autowired) in [PlanetService]

--- Testes/sw-planet-api/src/main/java/com/example/swplanetapi/domain/PlanetService.java
+++ Testes/sw-planet-api/src/main/java/com/example/swplanetapi/domain/PlanetService.java
@@ -1,6 +1,5 @@
 package com.example.swplanetapi.domain;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.Optional;
 @Service
