5a417b2a9e0d2f689c1c2093b3c61f2cbcf0cb47
Matheus dos Santos <matheus.dso1996@gmail.com>
2024-02-13 16:16:39 -0300

    Remove unused imports (RequestMethod and RequestParam) in [PlanetController]

--- Testes/sw-planet-api/src/main/java/com/example/swplanetapi/web/PlanetController.java
+++ Testes/sw-planet-api/src/main/java/com/example/swplanetapi/web/PlanetController.java
@@ -6,8 +6,6 @@ import com.example.swplanetapi.domain.Planet;
 import com.example.swplanetapi.domain.PlanetService;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,9 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
-
-
 
 @RestController
 @RequestMapping("/planets")
