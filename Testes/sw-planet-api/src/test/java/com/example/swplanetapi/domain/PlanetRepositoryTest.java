05b223e0740c5956c978f6546b0029b70e6048e5
Matheus dos Santos <matheus.dso1996@gmail.com>
2024-02-26 20:38:20 -0300

    Implement "DataJpaTest" in "PlanetRepositoryTest".
    Introduce "PlanetRepositoryTest" class for integration tests. Added "H2" database to "pom.xml" dependencies for enhanced functionality.

---
+++ Testes/sw-planet-api/src/test/java/com/example/swplanetapi/domain/PlanetRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.example.swplanetapi.domain;
+
+import static com.example.swplanetapi.common.PlanetConstants.PLANET;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+@DataJpaTest
+public class PlanetRepositoryTest {
+    @Autowired
+    private PlanetRepository planetRepository;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Test
+    public void createPlanet_WithValidData_ReturnsPlanet() {
+        Planet planet = planetRepository.save(PLANET);
+
+        Planet sut = testEntityManager.find(Planet.class, planet.getId());
+
+        assertThat(sut).isNotNull();
+        assertThat(sut.getName()).isEqualTo(PLANET.getName());
+        assertThat(sut.getClimate()).isEqualTo(PLANET.getClimate());
+        assertThat(sut.getTerrain()).isEqualTo(PLANET.getTerrain());
+    }
+}
