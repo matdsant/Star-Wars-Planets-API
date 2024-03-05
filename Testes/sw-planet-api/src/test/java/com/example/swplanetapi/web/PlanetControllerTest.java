a5bc0b101c92b7e45b44148a0d45330b66a9a5f6
Matheus dos Santos <matheus.dso1996@gmail.com>
2024-03-04 21:26:57 -0300

    Create test "createPlanet_WithValidData_ReturnsCreated". Implement test using "WebMvcTest," "ObjectMapper," and "MockBean" in the testing environment.

---
+++ Testes/sw-planet-api/src/test/java/com/example/swplanetapi/web/PlanetControllerTest.java
@@ -0,0 +1,39 @@
+package com.example.swplanetapi.web;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static com.example.swplanetapi.common.PlanetConstants.PLANET;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.example.swplanetapi.domain.PlanetService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@WebMvcTest(PlanetController.class)
+public class PlanetControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private PlanetService planetService;
+
+    @Test
+    public void createPlanet_WithValidData_ReturnsCreated() throws Exception {
+        when(planetService.create(PLANET)).thenReturn(PLANET);
+
+        mockMvc.perform(post("/planets").content(objectMapper.writeValueAsString(PLANET)).contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("$").value(PLANET));
+
+    }
+}
