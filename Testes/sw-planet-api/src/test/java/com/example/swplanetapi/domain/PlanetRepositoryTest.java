3c9b7ecf6b53a15b15441379f301bcc6d9097add
Matheus dos Santos <matheus.dso1996@gmail.com>
2024-03-09 15:26:37 -0300

    Completed Exercise Six

--- Testes/sw-planet-api/src/test/java/com/example/swplanetapi/domain/PlanetRepositoryTest.java
+++ Testes/sw-planet-api/src/test/java/com/example/swplanetapi/domain/PlanetRepositoryTest.java
@@ -74,4 +74,21 @@ public class PlanetRepositoryTest {
 
         assertThat(planetOpt).isEmpty();
     }
+
+    @Test
+	public void getPlanet_ByExistingName_ReturnsPlanet() {
+        Planet planet = testEntityManager.persistFlushFind(PLANET);
+
+        Optional<Planet> planetOpt = planetRepository.findByName(planet.getName());
+
+        assertThat(planetOpt).isNotEmpty();
+        assertThat(planetOpt.get()).isEqualTo(planet);
+    }
+
+    @Test
+    public void getPlanet_ByUnexistingName_ReturnsEmpty() {
+        Optional<Planet> planetOpt = planetRepository.findByName("name");
+
+        assertThat(planetOpt).isEmpty();
+    }
 }
--- Testes/sw-planet-api/src/test/java/com/example/swplanetapi/web/PlanetControllerTest.java
+++ Testes/sw-planet-api/src/test/java/com/example/swplanetapi/web/PlanetControllerTest.java
@@ -86,4 +86,21 @@ public class PlanetControllerTest {
 				.perform(get("/planets/1"))
 				.andExpect(status().isNotFound());
 	}
+
+	@Test
+	public void getPlanet_ByExistingName_ReturnsPlanet() throws Exception {
+		when(planetService.getByName(PLANET.getName())).thenReturn(Optional.of(PLANET));
+
+		mockMvc
+				.perform(get("/planets/name/" + PLANET.getName()))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$").value(PLANET));
+	}
+
+	@Test
+	public void getPlanet_ByUnexistingName_ReturnsNotFound() throws Exception {
+		mockMvc
+				.perform(get("/planets/name/1"))
+				.andExpect(status().isNotFound());
+	}
 }
