2343168a374c73f7658d817617424dca473e3fa8
Matheus dos Santos <matheus.dso1996@gmail.com>
2024-03-14 22:22:17 -0300

    Refactoring

--- Testes/sw-planet-api/src/main/java/com/example/swplanetapi/domain/Planet.java
+++ Testes/sw-planet-api/src/main/java/com/example/swplanetapi/domain/Planet.java
@@ -13,75 +13,76 @@ import org.apache.commons.lang3.builder.EqualsBuilder;
 @Entity
 @Table(name = "planets")
 public class Planet {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @NotEmpty
-    @Column(nullable = false, unique = true)
-    private String name;
+  @NotEmpty
+  @Column(nullable = false, unique = true)
+  private String name;
 
-    @NotEmpty
-    @Column(nullable = false)
-    private String climate;
+  @NotEmpty
+  @Column(nullable = false)
+  private String climate;
 
-    @NotEmpty
-    @Column(nullable = false)
-    private String terrain;
+  @NotEmpty
+  @Column(nullable = false)
+  private String terrain;
 
-    public Planet() {
-    }
+  public Planet() {
+  }
 
-    public Planet(String climate, String terrain) {
-        this.climate = climate;
-        this.terrain = terrain;
-    }
+  public Planet(String climate, String terrain) {
+    this.climate = climate;
+    this.terrain = terrain;
+  }
 
-    public Planet(String name, String climate, String terrain) {
-        this.name = name;
-        this.climate = climate;
-        this.terrain = terrain;
-    }
+  public Planet(String name, String climate, String terrain) {
+    this.name = name;
+    this.climate = climate;
+    this.terrain = terrain;
+  }
 
-    public Long getId() {
-        return id;
-    }
+  public Long getId() {
+    return id;
+  }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public String getClimate() {
-        return climate;
-    }
+  public String getClimate() {
+    return climate;
+  }
 
-    public void setClimate(String climate) {
-        this.climate = climate;
-    }
+  public void setClimate(String climate) {
+    this.climate = climate;
+  }
 
-    public String getTerrain() {
-        return terrain;
-    }
+  public String getTerrain() {
+    return terrain;
+  }
 
-    public void setTerrain(String terrain) {
-        this.terrain = terrain;
-    }
+  public void setTerrain(String terrain) {
+    this.terrain = terrain;
+  }
 
-    @Override
-    public boolean equals(Object obj) {
-        return EqualsBuilder.reflectionEquals(obj, this);
-    }
+  @Override
+  public boolean equals(Object obj) {
+    return EqualsBuilder.reflectionEquals(obj, this);
+  }
 
-    @Override
-    public String toString() {
-        return "Planet [id=" + id + ", name=" + name + ", climate=" + climate + ", terrain=" + terrain + "]";
-    }
+  @Override
+  public String toString() {
+    return "Planet [climate=" + climate + ", id=" + id + ", name=" + name + ", terrain=" + terrain + "]";
+  }
+
 }
--- Testes/sw-planet-api/src/test/java/com/example/swplanetapi/web/PlanetControllerTest.java
+++ Testes/sw-planet-api/src/test/java/com/example/swplanetapi/web/PlanetControllerTest.java
@@ -1,15 +1,14 @@
 package com.example.swplanetapi.web;
 
+import static com.example.swplanetapi.common.PlanetConstants.PLANET;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.Optional;
-
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static com.example.swplanetapi.common.PlanetConstants.PLANET;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,82 +24,79 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @WebMvcTest(PlanetController.class)
 public class PlanetControllerTest {
-	@Autowired
-	private MockMvc mockMvc;
+  @Autowired
+  private MockMvc mockMvc;
 
-	@Autowired
-	private ObjectMapper objectMapper;
+  @Autowired
+  private ObjectMapper objectMapper;
 
-	@MockBean
-	private PlanetService planetService;
+  @MockBean
+  private PlanetService planetService;
 
-	@Test
-	public void createPlanet_WithValidData_ReturnsCreated() throws Exception {
-		when(planetService.create(PLANET)).thenReturn(PLANET);
+  @Test
+  public void createPlanet_WithValidData_ReturnsCreated() throws Exception {
+    when(planetService.create(PLANET)).thenReturn(PLANET);
 
-		mockMvc.perform(post("/planets").content(objectMapper.writeValueAsString(PLANET))
-				.contentType(MediaType.APPLICATION_JSON))
-				.andExpect(status().isCreated())
-				.andExpect(jsonPath("$").value(PLANET));
-	}
+    mockMvc
+        .perform(
+            post("/planets").content(objectMapper.writeValueAsString(PLANET))
+                .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isCreated())
+        .andExpect(jsonPath("$").value(PLANET));
+  }
 
-	@Test
-	public void createPlanet_WithInvalidData_ReturnsBadRequest() throws Exception {
-		Planet emptyPlanet = new Planet();
-		Planet invalidPlanet = new Planet("", "", "");
+  @Test
+  public void createPlanet_WithInvalidData_ReturnsBadRequest() throws Exception {
+    Planet emptyPlanet = new Planet();
+    Planet invalidPlanet = new Planet("", "", "");
 
-		mockMvc
-				.perform(post("/planets").content(objectMapper.writeValueAsString(emptyPlanet))
-						.contentType(MediaType.APPLICATION_JSON))
-				.andExpect(status().isUnprocessableEntity());
+    mockMvc
+        .perform(
+            post("/planets").content(objectMapper.writeValueAsString(emptyPlanet))
+                .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isUnprocessableEntity());
+    mockMvc
+        .perform(
+            post("/planets").content(objectMapper.writeValueAsString(invalidPlanet))
+                .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isUnprocessableEntity());
+  }
 
-		mockMvc
-				.perform(post("/planets").content(objectMapper.writeValueAsString(invalidPlanet))
-						.contentType(MediaType.APPLICATION_JSON))
-				.andExpect(status().isUnprocessableEntity());
-	}
+  @Test
+  public void createPlanet_WithExistingName_ReturnsConflict() throws Exception {
+    when(planetService.create(any())).thenThrow(DataIntegrityViolationException.class);
 
-	@Test
-	public void createPlanet_WithExistingName_ReturnsConflict() throws Exception {
-		when(planetService.create(any())).thenThrow(DataIntegrityViolationException.class);
+    mockMvc
+        .perform(
+            post("/planets").content(objectMapper.writeValueAsString(PLANET))
+                .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isConflict());
+  }
 
-		mockMvc
-				.perform(post("/planets").content(objectMapper.writeValueAsString(PLANET))
-						.contentType(MediaType.APPLICATION_JSON))
-				.andExpect(status().isConflict());
-	}
+  @Test
+  public void getPlanet_ByExistingId_ReturnsPlanet() throws Exception {
+    when(planetService.get(1L)).thenReturn(Optional.of(PLANET));
 
-	@Test
-	public void getPlanet_ByExistingId_ReturnsPlanet() throws Exception {
-		when(planetService.get(1L)).thenReturn(Optional.of(PLANET));
+    mockMvc
+        .perform(
+            get("/planets/1"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$").value(PLANET));
+  }
 
-		mockMvc
-				.perform(get("/planets/1"))
-				.andExpect(status().isOk())
-				.andExpect(jsonPath("$").value(PLANET));
-	}
+  @Test
+  public void getPlanet_ByUnexistingId_ReturnsNotFound() throws Exception {
+    mockMvc.perform(get("/planets/1"))
+        .andExpect(status().isNotFound());
+  }
 
-	@Test
-	public void getPlanet_ByUnexistingId_ReturnsNotFound() throws Exception {
-		mockMvc
-				.perform(get("/planets/1"))
-				.andExpect(status().isNotFound());
-	}
+  @Test
+  public void getPlanet_ByExistingName_ReturnsPlanet() throws Exception {
+    // TODO implement
+  }
 
-	@Test
-	public void getPlanet_ByExistingName_ReturnsPlanet() throws Exception {
-		when(planetService.getByName(PLANET.getName())).thenReturn(Optional.of(PLANET));
-
-		mockMvc
-				.perform(get("/planets/name/" + PLANET.getName()))
-				.andExpect(status().isOk())
-				.andExpect(jsonPath("$").value(PLANET));
-	}
-
-	@Test
-	public void getPlanet_ByUnexistingName_ReturnsNotFound() throws Exception {
-		mockMvc
-				.perform(get("/planets/name/1"))
-				.andExpect(status().isNotFound());
-	}
+  @Test
+  public void getPlanet_ByUnexistingName_ReturnsNotFound() throws Exception {
+    // TODO implement
+  }
 }
